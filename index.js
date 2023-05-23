533903624bed5254a0295916acf36ddeb83bb8a2
Aleksandra Ujvari <ujvari65@gmail.com>
2023-05-23 15:04:16 +0200

    Add functionality for add to cart button

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let carts = document.querySelectorAll('.add-cart');
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers();
+    })
+}
+
 let removeCartItemButtons = document.getElementsByClassName('fa-trash-can')
 for (let i = 0; i < removeCartItemButtons.length; i++) {
     let button = removeCartItemButtons[i]
