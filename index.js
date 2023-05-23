a56ebe4a919f16896baefd7682cd27e6d53579d5
Aleksandra Ujvari <ujvari65@gmail.com>
2023-05-23 14:39:48 +0200

    Add functionality for trash-can icon

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-/* eslint-disable no-unused-vars */
-
-function sayHello() {
-  console.log('Hello, world!');
+let removeCartItemButtons = document.getElementsByClassName('fa-trash-can')
+for (let i = 0; i < removeCartItemButtons.length; i++) {
+    let button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event) {
+        let buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.parentElement.remove()
+    })
 }
+
