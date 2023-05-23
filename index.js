b8f844b798ec7d7d15a039d2694ea824e8a6022c
Aleksandra Ujvari <ujvari65@gmail.com>
2023-05-23 15:41:15 +0200

    Add function to save to local storage number of product in cart

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-let carts = document.querySelectorAll('.add-cart');
+const carts = document.querySelectorAll('.add-cart');
 for (let i = 0; i < carts.length; i++) {
-    carts[i].addEventListener('click', () => {
-        cartNumbers();
-    })
+  carts[i].addEventListener('click', () => {
+    cartNumbers();
+  });
 }
 
-let removeCartItemButtons = document.getElementsByClassName('fa-trash-can')
+function onLoadCartNumbers() {
+  const productNumbers = localStorage.getItem('cartNumbers');
+  if (productNumbers) {
+    document.querySelector('.cart span').textContent = productNumbers;
+  }
+}
+
+function cartNumbers() {
+  let productNumbers = localStorage.getItem('cartNumbers');
+  productNumbers = parseInt(productNumbers);
+  if (productNumbers) {
+    localStorage.setItem('cartNumbers', productNumbers + 1);
+    document.querySelector('.cart span').textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem('cartNumbers', 1);
+    document.querySelector('.cart span').textContent = 1;
+  }
+}
+
+onLoadCartNumbers();
+
+const removeCartItemButtons = document.getElementsByClassName('fa-trash-can');
 for (let i = 0; i < removeCartItemButtons.length; i++) {
-    let button = removeCartItemButtons[i]
-    button.addEventListener('click', function(event) {
-        let buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.parentElement.remove()
-    })
+  const button = removeCartItemButtons[i];
+  button.addEventListener('click', (event) => {
+    const buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.parentElement.remove();
+  });
 }
-
